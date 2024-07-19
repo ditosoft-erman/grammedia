428d804c8530f410e02ee01540110570246d0189
NARRA\emman <ermanfaminiano020@gmail.com>
2024-07-19 11:23:54 +0800

    update

--- src/pages/Offers.tsx
+++ src/pages/Offers.tsx
@@ -1,14 +1,12 @@
 import React from 'react'
 import "./about.css"
-import CardData from "../../src/data/CardData.json";
-
-
 export const Offers: React.FC = () => {
   return (
     <div className="offer-intro grid grid-cols-1 lg:grid-cols-2 bg-black">
+
     <div className="mx-auto flex flex-col text-center justify-center tracking-wide text-white p-24">
       <h1 className="text-4xl font-bold p-4">What <span className='gradient-text'>Gram Media</span> offers?</h1>
-  
+
       <p className="font-text xxxs xxxs:w-9/12 xxs:w-full md: xl:text-xl lg:w-3/4 text-justify mx-auto">
         At Gram Media, unlock the power of captivating storytelling through expert
         video editing. Transforming concepts into compelling visual narratives is
@@ -19,18 +17,30 @@ export const Offers: React.FC = () => {
     </div>
     <div className="card bg-black">
       <div className="cards flex flex-wrap gap-8 m-32 xl:m-20">
-          {
-            CardData.map((card, index) =>(
-              <div
-          key={index}
-          className={`w-52 h-52 bg-${card.className} bg-cover bg-white rounded-md px-16 flex justify-center items-center`}
-        ></div>
-            ))
-          }
+        <div
+          className="w-52 h-52 bg-viral-editing bg-cover bg-white px-16 rounded-md flex justify-center items-center"
+        >
+     
+        </div>
+        <div
+          className="w-52 h-52 bg-sculpting bg-cover bg-white rounded-md px-16 flex justify-center items-center"
+        >
+  
+        </div>
+        <div
+          className="w-52 h-52 bg-partnership bg-cover bg-white rounded-md px-16 flex justify-center items-center"
+        >
+  
+        </div>
+        <div
+          className="w-52 h-52 bg-specific bg-cover bg-white rounded-md px-16 flex justify-center items-center"
+        >
+    
         </div>
       </div>
     </div>
+  </div>
+  
   );
 };
-
 export default Offers;
---
+++ src/pages/Project.tsx
@@ -0,0 +1,114 @@
+import React, { useState, useRef } from "react";
+import vidReel from "../../src/assets/reels/hormozi.mp4";
+import vidReel2 from "../../src/assets/reels/hormozi-2.mp4";
+import vidReel3 from "../../src/assets/reels/hormozi-3.mp4";
+import vidReel4 from "../../src/assets/reels/hormozi-4.mp4";
+
+interface Project {
+  id: number;
+  title: string;
+  videoSrc: string;
+}
+
+const Projects: React.FC = () => {
+  const projectData: Project[] = [
+    { id: 1, title: "Project 1", videoSrc: vidReel },
+    { id: 2, title: "Project 2", videoSrc: vidReel2 },
+    { id: 3, title: "Project 3", videoSrc: vidReel3 },
+    { id: 4, title: "Project 4", videoSrc: vidReel4 },
+  ];
+
+  const [playingVideoId, setPlayingVideoId] = useState<number | null>(null);
+  const videoRefs = useRef<(HTMLVideoElement | null)[]>([]);
+
+  const handlePlay = (id: number) => {
+
+    if (playingVideoId !== null && playingVideoId !== id) {
+      const currentVideo = videoRefs.current[playingVideoId];
+      if (currentVideo) {
+        currentVideo.pause();
+        currentVideo.currentTime = 0; 
+      }
+    }
+
+    setPlayingVideoId(id);
+  };
+
+  return (
+    <div className="bg-black flex flex-col">
+      <h1 className="gradient-text font-bold font-sub xxxs:text-3xl md:text-7xl text-center">
+        Projects
+      </h1>
+      <div className="flex md:flex-wrap justify-center mt-5 items-center h-screen relative overflow-x-auto">
+        <div className="flex md:flex-wrap flex-nowrap xxxs:space-x-2 sm:space-x-4">
+          {projectData.map((project) => (
+            <div
+              key={project.id}
+              className="bg-reels bg-contain bg-no-repeat w-[15rem] h-[26rem] flex justify-center items-center text-xl text-center rounded-md m-2 transition-transform duration-300 transform hover:scale-105 hover:border-white hover:border-4"
+            >
+              <video
+                className="w-[12.5rem] h-[21rem]"
+                controls
+                ref={(el) => (videoRefs.current[project.id] = el)}
+                onPlay={() => handlePlay(project.id)}
+              >
+                <source src={project.videoSrc} type="video/mp4" />
+              </video>
+            </div>
+          ))}
+        </div>
+        <div className="flex md:flex-wrap flex-nowrap xxxs:space-x-2 sm:space-x-4">
+          {projectData.map((project) => (
+            <div
+              key={project.id}
+              className="bg-reels bg-contain bg-no-repeat w-[15rem] h-[26rem] flex justify-center items-center text-xl text-center rounded-md m-2 transition-transform duration-300 transform hover:scale-105 hover:border-white hover:border-4"
+            >
+              <video
+                className="w-[12.5rem] h-[21rem]"
+                controls
+                ref={(el) => (videoRefs.current[project.id] = el)}
+                onPlay={() => handlePlay(project.id)}
+              >
+                <source src={project.videoSrc} type="video/mp4" />
+              </video>
+            </div>
+            
+          ))}
+        </div>
+        <div className="flex md:flex-wrap flex-nowrap xxxs:space-x-2 sm:space-x-4">
+          {projectData.map((project) => (
+            <div
+              key={project.id}
+              className="bg-reels bg-contain bg-no-repeat w-[15rem] h-[26rem] flex justify-center items-center text-xl text-center rounded-md m-2 transition-transform duration-300 transform hover:scale-105 hover:border-white hover:border-4"
+            >
+              <video
+                className="w-[12.5rem] h-[21rem]"
+                controls
+                ref={(el) => (videoRefs.current[project.id] = el)}
+                onPlay={() => handlePlay(project.id)}
+              >
+                <source src={project.videoSrc} type="video/mp4" />
+              </video>
+            </div>
+          ))}
+        </div>
+        <div className="flex md:flex-wrap flex-nowrap xxxs:space-x-2 sm:space-x-4">
+          {projectData.map((project) => (
+            <div className="bg-reels bg-contain bg-no-repeat w-[15rem] h-[26rem] flex justify-center items-center text-xl text-center round-md m-2 transition-transform duration-300 transform hover:scale-105 hover:border-white hover:border-4">
+              <video
+                className="w-[12.5rem] h-[21rem]"
+                controls
+                ref={(el) => (videoRefs.current[project.id] = el)}
+                onPlay={() => handlePlay(project.id)}
+              >
+                <source src={project.videoSrc} type="video/mp4" />
+              </video>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Projects;
