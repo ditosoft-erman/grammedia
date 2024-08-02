7c954754ff4f581667a2d5453279d5a352a4eab5
narra <ermanfaminiano020@gmail.com>
2024-08-02 15:25:05 +0800

    feat: :lipstick: remove stroke in text

--- src/pages/Testimony.tsx
+++ src/pages/Testimony.tsx
@@ -2,73 +2,66 @@ import { FC, useState, useEffect } from "react";
 
 import test from "../data/test.json";
 import { Parallax, ParallaxProvider } from "react-scroll-parallax";
-import  "../pages/about.css"
+import "../pages/about.css";
 
 export const Testimony: FC = () => {
-	// eslint-disable-next-line @typescript-eslint/no-unused-vars
-	const [_, setIsOnPage] = useState(false);
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [_, setIsOnPage] = useState(false);
 
-	useEffect(() => {
-		const handleScroll = () => {
-			const testimonyElement = document.getElementById("testimony");
-			if (testimonyElement) {
-				const rect = testimonyElement.getBoundingClientRect();
-				setIsOnPage(rect.top <= window.innerHeight && rect.bottom >= 0);
-			}
-		};
+  useEffect(() => {
+    const handleScroll = () => {
+      const testimonyElement = document.getElementById("testimony");
+      if (testimonyElement) {
+        const rect = testimonyElement.getBoundingClientRect();
+        setIsOnPage(rect.top <= window.innerHeight && rect.bottom >= 0);
+      }
+    };
 
-		window.addEventListener("scroll", handleScroll);
+    window.addEventListener("scroll", handleScroll);
 
-		handleScroll();
+    handleScroll();
 
-		return () => {
-			window.removeEventListener("scroll", handleScroll);
-		};
-	}, []);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
-	return (
-		<div
-			className='w-full h-full bg-black'
-			id='testimony'>
-			<div className='container'>
-				<h1 className='gradient-text font-bold p-28 font-sub xxxs:text-3xl  md:text-7xl text-center '>
-					Testimonials
-				</h1>
-			
-				<div className='xxxs:mt-14 p-0 md:mt-30 px-10'>
-					<div className='flex flex-col gap-40'>
-						{test.map((testimony, idx) => (
-							<ParallaxProvider key={idx}>
-								<Parallax
-									speed={1}
-									
-									translateX={["-300px", "1000px"]}>
-									<div
-										id='testimony'
-										className='flex flex-col gap-4 lg:px-36'>
-										<div className='flex gap-10'>
-											<h1 className='xxxs:text-white text-[100px] font-span'>
-												"
-											</h1>
-											<div className='flex flex-col '>
-												<h1
-													className={` text-white font-sub text-justify text-stroke-1 text-stroke-gram xxxs:text-lg tracking-tighter md:text-3xl lg:text-6xl
-													} hover:text-secondary `}>
-													{testimony.test}
-												</h1>
-												
-											</div>
-											<h1 className='xxxs:text-white font-bold text-[100px] font-span'>
-												"
-											</h1>
-										</div>
-									</div>
-								</Parallax>
-							</ParallaxProvider>
-						))}
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="w-full h-full bg-black" id="testimony">
+      <div className="container">
+        <h1 className="gradient-text font-bold p-28 font-sub xxxs:text-3xl  md:text-7xl text-center ">
+          Testimonials
+        </h1>
+
+        <div className="xxxs:mt-14 p-0 md:mt-30 px-10">
+          <div className="flex flex-col gap-40">
+            {test.map((testimony, idx) => (
+              <ParallaxProvider key={idx}>
+                <Parallax speed={1} translateX={["-300px", "1000px"]}>
+                  <div id="testimony" className="flex flex-col gap-4 lg:px-36">
+                    <div className="flex gap-10">
+                      <h1 className="xxxs:text-white text-[100px] font-span">
+                        "
+                      </h1>
+                      <div className="flex flex-col ">
+                        <h1
+                          className={` text-white font-sub text-justify xxxs:text-lg tracking-tighter md:text-3xl lg:text-6xl
+													} hover:text-secondary `}
+                        >
+                          {testimony.test}
+                        </h1>
+                      </div>
+                      <h1 className="xxxs:text-white font-bold text-[100px] font-span">
+                        "
+                      </h1>
+                    </div>
+                  </div>
+                </Parallax>
+              </ParallaxProvider>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
