f45a94b22807343ab83c99c63c168b750622f38c
narra <ermanfaminiano020@gmail.com>
2024-08-03 13:33:13 +0800

    feat: :zap: new update

--- src/pages/Project.tsx
+++ src/pages/Project.tsx
@@ -4,33 +4,38 @@ interface Project {
   id: number;
   title: string;
   videoSrc: string;
+  link: string;
 }
 
 const Projects: React.FC = () => {
   const reels: Project[] = [
     {
       id: 1,
-      title: "Project 1",
+      title: "Reels",
       videoSrc:
         "https://grammedia-vids.s3.ap-southeast-2.amazonaws.com/hormozi-1.mp4",
+      link: "https://drive.google.com/drive/folders/1z6sQCVm4bWMFodoepm6gFn2ZNwfsnbUX?usp=sharing",
     },
     {
       id: 2,
-      title: "",
+      title: "Real Estate",
       videoSrc:
         "https://grammedia-vids.s3.ap-southeast-2.amazonaws.com/real-estate-1.mp4",
+      link: "https://drive.google.com/drive/folders/1PNSrCVg3bXE3oIKIXFBovhjJTrllvMaJ?usp=sharing",
     },
     {
       id: 3,
-      title: "",
+      title: "Podcast",
       videoSrc:
         "https://grammedia-vids.s3.ap-southeast-2.amazonaws.com/podcast-1.mp4",
+      link: "https://drive.google.com/drive/folders/1sJUxcPYRW3dsa3KXOCkRhzb9u50-MI8q?usp=drive_link",
     },
     {
       id: 4,
-      title: "",
+      title: "Faceless",
       videoSrc:
         "https://grammedia-vids.s3.ap-southeast-2.amazonaws.com/faceless-1.mp4",
+      link: "https://drive.google.com/drive/folders/1bc09dB7Ce_W2BpQ3lT6Aqrq5x5-0Z9xT?usp=sharing",
     },
   ];
 
@@ -57,18 +62,31 @@ const Projects: React.FC = () => {
       <div className="relative w-screen overflow-x-auto mt-5">
         <div className="flex flex-nowrap justify-center items-center space-x-10 min-w-max mt-5 p-5">
           {reels.map((project) => (
-            <div
-              key={project.id}
-              className="bg-reels bg-cover bg-no-repeat w-[16rem] h-[28rem] flex justify-center items-center text-xl text-center rounded-md transition-transform duration-300 transform hover:scale-105 hover:border-white hover:border-4"
-            >
-              <video
-                className="w-auto h-full object-cover p-2"
-                controls
-                ref={(el) => (videoRefs.current[project.id] = el)}
-                onPlay={() => handlePlay(project.id)}
+            <div className="innerHeight">
+              <div
+                key={project.id}
+                className="bg-reels bg-cover bg-no-repeat w-[16rem] h-[28rem] flex justify-center items-center text-xl text-center rounded-md transition-transform duration-300 transform hover:scale-105 hover:border-white hover:border-4"
               >
-                <source src={project.videoSrc} type="video/mp4" />
-              </video>
+                <video
+                  className="w-auto h-full object-cover p-2"
+                  controls
+                  ref={(el) => (videoRefs.current[project.id] = el)}
+                  onPlay={() => handlePlay(project.id)}
+                >
+                  <source src={project.videoSrc} type="video/mp4" />
+                </video>
+              </div>
+              <div className="flex justify-center items-center p-3 ">
+                <button className="border text-white border-cyan-300 rounded-md p-2 px-6 hover:bg-cyan-300">
+                  <a
+                    className="underline:none hover:underline:none"
+                    href={project.link}
+                    target="_blank"
+                  >
+                    {project.title}
+                  </a>
+                </button>
+              </div>
             </div>
           ))}
         </div>
--- src/pages/PromotionalVideo.tsx
+++ src/pages/PromotionalVideo.tsx
@@ -1,12 +1,14 @@
-import React from 'react';
-import videoSource from '../assets/video/promotional.mp4';
+import React from "react";
 
 export const PromotionalVideo: React.FC = () => {
   return (
     <div className="flex justify-center items-center h-screen bg-black">
       <div className="max-w-6xl max-h-5xl">
         <video autoPlay controls className="w-full h-full object-contain">
-          <source src={videoSource} type="video/mp4" />
+          <source
+            src="https://grammedia-vids.s3.ap-southeast-2.amazonaws.com/promotional.mp4"
+            type="video/mp4"
+          />
         </video>
       </div>
     </div>
